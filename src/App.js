541b8e73ab47ae5cdae133f12c6cc32f9e98aea2
Vilin Kehayov <112933705+VilinKehayov@users.noreply.github.com>
2023-03-17 14:40:03 +0200

    React Router imported, started with diff routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route
+} from "react-router-dom";
+
+import Register from "./pages/Register"
+import Login from "./pages/Login"
+import Write from "./pages/Write"
+import Sigle from "./pages/Sigle"
+import Home from "./pages/Home"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>This is Home Page!!</div>,
+  },
+  {
+    path: "/register",
+    element: <Register/>,
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+]);
+
+
 function App() {
   return (
     <div>
-      Hello world!
+      <RouterProvider router={router}/>
     </div>
   );
 }
