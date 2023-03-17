d65209e06b62f3e73bf25233a8cabac4e4beaa73
Vilin Kehayov <112933705+VilinKehayov@users.noreply.github.com>
2023-03-17 14:58:26 +0200

    Still making different pages and routes

--- src/App.js
+++ src/App.js
@@ -1,35 +1,58 @@
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Route, Outlet } from "react-router-dom";
 
-import Register from "./pages/Register"
-import Login from "./pages/Login"
-import Write from "./pages/Write"
-import Sigle from "./pages/Sigle"
-import Home from "./pages/Home"
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Write from "./pages/Write";
+import Single from "./pages/Single";
+import Home from "./pages/Home";
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>This is Home Page!!</div>,
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/post/:id",
+        element: <Single />,
+      },
+      {
+        path: "/write",
+        element: <Write />,
+      },
+    ],
   },
   {
     path: "/register",
-    element: <Register/>,
+    element: <Register />,
   },
   {
     path: "/login",
-    element: <Login/>,
+    element: <Login />,
   },
 ]);
 
-
 function App() {
   return (
-    <div>
-      <RouterProvider router={router}/>
+    <div className="app">
+      <div className="container"></div>
+      <RouterProvider router={router} />
     </div>
   );
 }
