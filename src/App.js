ef86e7b12fa90a912e044aeeee812eda5aed4c9b
Vilin Kehayov <112933705+VilinKehayov@users.noreply.github.com>
2023-03-20 08:46:55 +0200

    Styles imported, Login page styled..

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import Home from "./pages/Home";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import "./style.scss"
 
 const Layout = () => {
   return (
