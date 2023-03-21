44490e708e8076ec3e4aef2a6cdd3d07e35c0382
Vilin Kehayov <112933705+VilinKehayov@users.noreply.github.com>
2023-03-21 14:06:44 +0200

    Started styling navbar component..

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import Logo from "../img/logo.png"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="navbar">
+      <div className="container">
+        <div className="logo">
+          <img src={Logo} alt="" />
+        </div>
+        <div className="links">links</div>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
