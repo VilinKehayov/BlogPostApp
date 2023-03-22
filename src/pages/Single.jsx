9ea789c6996d2f1ad3d494170ead2c3c6c93b667
Vilin Kehayov <112933705+VilinKehayov@users.noreply.github.com>
2023-03-22 15:37:46 +0200

    Started building sigle page and styling for it

--- src/pages/Single.jsx
+++ src/pages/Single.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const Single = () => {
   return (
-    <div>Single</div>
-  )
-}
+    <div className="single">
+      <div className="content">
+        <img
+          src="https://www.shutterstock.com/image-photo/man-traveler-hiking-alone-breathtaking-landscape-1092776753"
+          alt=""
+        />
+        <div className="user">
+          <img
+            src="https://www.shutterstock.com/image-photo/small-kettle-heated-on-bonfire-hiking-1822074260"
+            alt=""
+          />
+          <div className="info">
+            <span>Vilin</span>
+            <p>Posted 1 day ago</p>
+          </div>
+        </div>
+      </div>
+      <div className="menu">m</div>
+    </div>
+  );
+};
 
-export default Single
+export default Single;
