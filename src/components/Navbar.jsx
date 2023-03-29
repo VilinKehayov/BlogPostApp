dd194d04c03167822831f5b9493c87301b3c29b0
Vilin Kehayov <112933705+VilinKehayov@users.noreply.github.com>
2023-03-29 08:48:29 +0300

    Link add to the logo, started to fetch real data.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,17 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import Logo from "../img/logo.png";
 import { Link } from "react-router-dom";
-import {AuthContext} from "../context/authContext.js"
+import { AuthContext } from "../context/authContext.js";
 
 const Navbar = () => {
-
-  const {currentUser, logout} = useContext(AuthContext)
+  const { currentUser, logout } = useContext(AuthContext);
   return (
     <div className="navbar">
       <div className="container">
         <div className="logo">
-          <img src={Logo} alt="" />
+          <Link to="/">
+            <img src={Logo} alt="" />
+          </Link>
         </div>
         <div className="links">
           <Link className="link" to="/?cat=art">
@@ -32,9 +33,17 @@ const Navbar = () => {
             <h6>FOOD</h6>
           </Link>
           <span>{currentUser?.username}</span>
-          {currentUser ? <span>Logout</span> : <Link className="link" to="/login">Login</Link>}
+          {currentUser ? (
+            <span onClick={logout}>Logout</span>
+          ) : (
+            <Link className="link" to="/login">
+              Login
+            </Link>
+          )}
           <span className="write">
-            <Link className="link" to="write">Write</Link>
+            <Link className="link" to="write">
+              Write
+            </Link>
           </span>
         </div>
       </div>
